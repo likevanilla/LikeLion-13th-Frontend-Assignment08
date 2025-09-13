9044296915b22af64242350a7a6b0105556313e1
likevanilla <wjdgur27507@gmail.com>
2025-09-13 17:15:55 +0900

    Pretendard SemiBold 글꼴 적용

--- Leejeonghyuk/portfolio/src/GlobalStyle.jsx
+++ Leejeonghyuk/portfolio/src/GlobalStyle.jsx
@@ -1,9 +1,17 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-    * {
+    @font-face {
+        font-family: 'Pretendard';
+        src: url("./src/fonts/Pretendard-SemiBold.woff") format('woff'),
+            url("./src/fonts/Pretendard-SemiBold.woff2") format()('woff2');
+        font-style: normal;
+    }
+
+
+    *, body {
         margin: 0;
         padding: 0;
-        font-family: 'Pretendard', sans-serif;
+        font-family: 'Pretendard';
     }
 `;
---
+++ Leejeonghyuk/portfolio/src/components/MySkills.jsx
@@ -0,0 +1,7 @@
+export default function MySkills() {
+  return (
+    <div className="skillSection">
+      <h2>SKILLS & TOOLS</h2>
+    </div>
+  );
+}
