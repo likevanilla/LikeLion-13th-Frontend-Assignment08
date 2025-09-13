d2185660e5e6ac6f8cc4b2d6a2b6e0de64c382bc
likevanilla <wjdgur27507@gmail.com>
2025-09-13 17:42:22 +0900

    skills 페이지 관련 이미지 추가, 스타일 적용 중

--- Leejeonghyuk/portfolio/src/components/MySkills.jsx
+++ Leejeonghyuk/portfolio/src/components/MySkills.jsx
@@ -1,7 +1,48 @@
+import Html from "../assets/icon_html.svg";
+import CSS from "../assets/icon_css.svg";
+import JavaScript from "../assets/icon_javascript.svg";
+import React from "../assets/icon_react.svg";
+import Figma from "../assets/icon_figma.svg";
+
 export default function MySkills() {
   return (
     <div className="skillSection">
-      <h2>SKILLS & TOOLS</h2>
+      <h2 className="skillTitle">SKILLS</h2>
+      <div className="skill">
+        <div className="skillBox">
+          <div className="skillName">
+            <img className="icons" src={Html} alt="Html 이미지" />
+            HTML
+          </div>
+        </div>
+        <div className="skillBox">
+          <div className="skillName">
+            <img className="icons" src={CSS} alt="CSS 이미지" />
+            CSS
+          </div>
+        </div>
+        <div className="skillBox">
+          <div className="skillName">
+            <img className="icons" src={JavaScript} alt="JavaScript 이미지" />
+            JAVASCRIPT
+          </div>
+        </div>
+        <div className="skillBox">
+          <div className="skillName">
+            <img className="icons" src={React} alt="React 이미지" />
+            REACT
+          </div>
+        </div>
+
+        <br />
+
+        <div className="skillBox">
+          <div className="skillName">
+            <img className="icons" src={Figma} alt="Figma 이미지" />
+            FIGMA
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- Leejeonghyuk/portfolio/src/pages/PortfolioPage.jsx
+++ Leejeonghyuk/portfolio/src/pages/PortfolioPage.jsx
@@ -1,11 +1,14 @@
 import MyProfile from "../components/MyProfile";
-import "../styles/PageStyle.css";
+import MySkills from "../components/MySkills";
+import "../styles/ProfileStyle.css";
+import "../styles/SkillsStyle.css";
 
 export default function PortfolioPage() {
   return (
     <>
       <div>
         <MyProfile />
+        <MySkills />
       </div>
     </>
   );
